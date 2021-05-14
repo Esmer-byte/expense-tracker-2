af10b53ffee977ac028eeb725cfc4ed835cd64c5
Esmer-byte <77016339+Esmer-byte@users.noreply.github.com>
2021-05-14 23:52:00 +0300

    Add files via upload

---
+++ list.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import "./list.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./filter";
+import { useState } from "react/cjs/react.development";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  function onChangeYearHandler(year) {
+    setFilteredYear(year);
+  }
+  const dummy = props.array.filter(expense => expense.date.getFullYear().toString() === filteredYear)
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeYear={onChangeYearHandler}
+        />
+        {dummy.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
